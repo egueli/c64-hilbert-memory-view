d158ef690bc9fb2c78c42b3cb80ad9ce8429711d
Enrico Gueli <enrico.gueli@gmail.com>
2016-07-30 10:17:35 +0200

    fix missing zero-arg instructions, show register values, flag indirect indexed addressing

--- client.py
+++ client.py
@@ -23,11 +23,20 @@ def processLines(lines):
 
 	line = lines[0]
 	#print line
-	m = re.search('...([0-9a-f]+).{14}([A-Z]{3}) ([^ ]+).*  ([0-9]+)', line)
+	m = re.search('...([0-9a-f]+).{14}(.+) - A:(..) X:(..) Y:(..).* ([0-9]+)', line)
 	if not m:
 		return
 
-	print m.group(4), m.group(1), m.group(2), m.group(3)
+	groups = [m.group(i) for i in range(1, 7)]
+	ip, instruction, a, x, y, time = groups
+
+	im = re.search('... \(\$([^)]+)\),Y', instruction)
+	flags = ""
+	if not im:
+		print time, ip, instruction, a, x, y, flags
+	else:
+		flags = flags + "I"
+		print time, ip, instruction, a, x, y, flags
 
 
 TCP_IP = '127.0.0.1'
@@ -35,13 +44,13 @@ TCP_PORT = 6510
 MESSAGE = "\nreset\n"
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
 sock.connect((TCP_IP, TCP_PORT))
 sock.send(MESSAGE)
 
 for lines in readMonitorLine(sock):
 	processLines(lines);	
 	sock.send("step\n")
-
 
 
 sock.close()
