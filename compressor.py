3a2c7a97f743dda66972dc17c1dd4944f97df294
Enrico Gueli <enrico.gueli@gmail.com>
2016-08-05 21:40:21 +0200

    created compressor script (to avoid p5.js to handle a trace file too big)

---
+++ compressor.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+import sys
+
+class FrameData:
+	def __init__(self, sequence, time):
+		self.addresses = {}
+		self.sequence = sequence
+		self.time = time
+
+	def output(self):
+		s = []
+		s.append(self.time)
+		for direction in self.addresses:
+			s.append(direction)
+			accesses = list(set(self.addresses[direction]))
+			accesses.sort()
+
+			rangeStart = None
+			rangeLen = 0
+			for access in accesses:
+				if rangeStart is None:
+					rangeStart = access
+					rangeLen = 1
+				else:
+					if access == rangeStart + rangeLen:
+						rangeLen = rangeLen + 1
+					else:
+						s.append(rangeStart)
+						s.append(rangeLen)
+						rangeStart = access
+						rangeLen = 1
+			s.append(rangeStart)
+			s.append(rangeLen)
+		print " ".join([str(n) for n in s])
+
+
+	def parse(self, fields):
+		address = int(fields[1], 16)
+		direction = fields[2]
+		if not direction in self.addresses:
+			self.addresses[direction] = []
+
+		self.addresses[direction].append(address)
+
+
+f = FrameData(0, 0)
+
+frameDuration = 10
+currentFrame = 0
+startTime = None
+for line in sys.stdin:
+	try:
+		fields = line.split()
+		time = int(fields[0])
+
+		if not startTime:
+			startTime = time
+		else:
+			time = time - startTime
+
+		sequence = int(time / frameDuration)
+		if sequence != f.sequence:
+			f.output()
+			f = FrameData(sequence, time)
+
+		f.parse(fields)
+	except:
+		print "error while processing line " + line
+		raise
+
+output()
+
