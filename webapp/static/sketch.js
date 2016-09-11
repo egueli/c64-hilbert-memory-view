af3df4d5218e42a94860fa53dadd56f77ef2ff40
Enrico Gueli <enrico.gueli@gmail.com>
2016-09-11 20:20:51 +0200

    simple data-requesting loop

--- webapp/static/sketch.js
+++ webapp/static/sketch.js
@@ -1,11 +1,28 @@
+var currentTimestamp;
+var microsecondsPerFrame = 20000;
+
+var traceInfo;
+
 function preload() {
-	
+	traceInfo = loadJSON("/info");
+	setFrameRate(50);
 }
 
 function setup() {
-	
+	console.log(traceInfo);
+	currentTimestamp = traceInfo.firstTimestamp;
 }
 
+var needData = true;
+
 function draw() {
-	
+	if (needData) {
+		loadJSON("/trace?from=" + currentTimestamp + "&to=" + (currentTimestamp + microsecondsPerFrame), onDataForFrame);
+		needData = false;
+	}
+}
+
+function onDataForFrame(data) {
+	needData = true;
+	console.log(data);
 }
