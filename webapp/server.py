e69c3199938cefda5b04cb8203cb5369f26ee3c5
Enrico Gueli <enrico.gueli@gmail.com>
2016-09-20 21:05:09 +0200

    adhere to PEP 8

--- webapp/server.py
+++ webapp/server.py
@@ -15,19 +15,21 @@ Profiler(app)
 
 conn = sqlite3.connect('test.sqlite')
 
+
 @app.route('/')
 def home():
     print "root!"
     return app.send_static_file('index.html')
 
+
 @app.route('/<path:path>')
 def static_proxy(path):
-  # send_static_file will guess the correct MIME type
-  return app.send_static_file(path)
+    # send_static_file will guess the correct MIME type
+    return app.send_static_file(path)
 
 
 @app.route("/info")
-def getTraceInfo():
+def get_trace_info():
     first = conn.execute('SELECT timestamp FROM accesses ORDER BY timestamp ASC LIMIT 1').fetchone()[0]
     last = conn.execute('SELECT timestamp FROM accesses ORDER BY timestamp DESC LIMIT 1').fetchone()[0]
     out = {
@@ -41,10 +43,10 @@ def getTraceInfo():
 
 
 @app.route("/trace")
-def getTraceData():
-    fromTime = int(request.args.get('from'))
-    toTime = int(request.args.get('to')) + 1
-    accesses = get_accesses(fromTime, toTime)
+def get_trace_data():
+    from_time = int(request.args.get('from'))
+    to_time = int(request.args.get('to')) + 1
+    accesses = get_accesses(from_time, to_time)
 
     out = {
         'accesses': accesses
@@ -55,25 +57,25 @@ def getTraceData():
         return flask.jsonify(**out)
 
 
-def get_accesses(fromTime, toTime):
-    accCursor = conn.execute('SELECT type, address FROM accesses WHERE timestamp >= ? AND timestamp < ?',
-                             (fromTime, toTime))
-    accessesRaw = [{}, {}, {}]
+def get_accesses(from_time, to_time):
+    acc_cursor = conn.execute('SELECT type, address FROM accesses WHERE timestamp >= ? AND timestamp < ?',
+                             (from_time, to_time))
+    accesses_raw = [{}, {}, {}]
     while True:
-        fetched = accCursor.fetchmany(toTime - fromTime)
+        fetched = acc_cursor.fetchmany(to_time - from_time)
         if not fetched:
             break
         for row in fetched:
-            typeNum, address = row
-            accessesRaw[typeNum][address] = True
+            type_num, address = row
+            accesses_raw[type_num][address] = True
     accesses = {
         'read': [],
         'write': [],
         'execute': []
     }
     for i in range(0, 3):
-        accessType = ['read', 'write', 'execute'][i]
-        accesses[accessType] = list(accessesRaw[i])
+        access_type = ['read', 'write', 'execute'][i]
+        accesses[access_type] = list(accesses_raw[i])
     return accesses
 
 
